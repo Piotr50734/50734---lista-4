387cfbab8270335e220da83ec344f2640c3e84ab
Piotr Twardowski <131593610+Piotr50734@users.noreply.github.com>
2023-04-23 17:43:32 +0200

    aa

--- skrypt7.sh
+++ skrypt7.sh
@@ -1,12 +1,7 @@
-#!/bin/bash
 
 if [ "$1" = "--init" ]; then
-  git clone <https://github.com/Piotr50734/50734---lista-4>
-
-  echo "export PATH=$PATH:$(pwd)/skrypt" >> ~/.bashrc
-  source ~/.bashrc
-
-  echo "Repozytorium zostało sklonowane, a skrypt został dodany do PATH."
-else
-  echo "Nieznane polecenie. Użyj --help, aby wyświetlić dostępne opcje."
+  git clone https://github.com/Piotr50734/50734---lista-4
+  export PATH=$PATH:$(pwd)/skrypt
+  echo "Repozytorium zostało sklonowane do $(pwd)/skrypt, a ścieżka do folderu została dodana do PATH."
+  exit 0
 fi
