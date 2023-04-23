b7bb1606bffc5aaad5517b8cadf65419edd1eb1c
Piotr Twardowski <131593610+Piotr50734@users.noreply.github.com>
2023-04-23 17:27:14 +0200

    sss

--- skrypt6.sh
+++ skrypt6.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
-Usage: skrypt5.sh [OPTIONS]
 
-OPTIONS:
-  -h, --help        Display this help message
-  -l, --logs [NUM]  Create NUM log files with names logX.txt, where X is the number of the file. In each file, the script will write its name, the name of the script that created it and the date.
-  -d, --date        Display today's date
-  -v, --version     Display the version number
+if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
+  echo "Dostępne opcje:"
+  echo "--date - wyświetla dzisiejszą datę"
+  echo "--logs [liczba] - tworzy podaną liczbę plików logx.txt i wypełnia je informacjami"
+elif [ "$1" = "--logs" ] || [ "$1" = "-l" ]; then
+  if [ "$2" ]; then
+    for i in $(seq 1 "$2")
+    do
+      touch log"$i".txt
+      echo "Nazwa pliku: log$i.txt" >> log"$i".txt
+      echo "Nazwa skryptu: skrypt.sh" >> log"$i".txt
+      echo "Data utworzenia: $(date)" >> log"$i".txt
+    done
+    echo "Utworzono $2 plików logx.txt"
+  else
+    for i in {1..100}
+    do
+      touch log"$i".txt
+      echo "Nazwa pliku: log$i.txt" >> log"$i".txt
+      echo "Nazwa skryptu: skrypt.sh" >> log"$i".txt
+      echo "Data utworzenia: $(date)" >> log"$i".txt
+    done
+    echo "Utworzono 100 plików logx.txt"
+  fi
+elif [ "$1" = "--date" ] || [ "$1" = "-d" ]; then
+  echo "Dzisiejsza data to: $(date)"
+else
+  echo "Nieznane polecenie. Użyj --help lub -h, aby wyświetlić dostępne opcje."
+fi
